488be23892270fd40d776cc54a89a9600bd74af6
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2024-06-13 20:33:45 -0500

    update

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import numpy as np
 
+IP = "127.0.0.1"
+PORT = 5000
 BUTTON = "#000000"
 BACKGROUND = "#FFFFFF"
 TEXT = "#000000"
-COLOR = np.array([
-            [1, 0, 0],  # Red
-            [0, 1, 0],  # Green
-            [0, 0, 1],  # Blue
-            [1, 1, 0],  # Yellow
-            [1, 0, 1],  # Magenta
-            [0, 1, 1],  # Cyan
-            [0.5, 0, 0.5],  # Purple
-            [0.5, 0.5, 0],  # Olive
-            [0, 0.5, 0.5],  # Teal
-            [0.5, 0.5, 0.5]  # Gray
-        ])
+COLOR = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange', 'purple', 'brown']
 ABOUT_TXT = "Esta es una aplicación\npara la visualización de datos.\n\nDesarrollado por:\n\n- Bruno Fernández\n- Joaquín Pino\n- Fredy Quispe"
